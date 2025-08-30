581d84671cc991e4c585ad64ccbb629a171c20a4
Mironiusz <47088369+Mironiusz@users.noreply.github.com>
2025-08-30 15:25:02 +0200

    Filip TODO

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-print('test')
+if __name__ == "__main__":
+    #TODO Filip
+    """
+    Funkcje inicjalizujące class Poker i class Player.
+    Najpierw musimy utworzyć graczy, a potem poker.
+    Graczowi trzeba przypisać name: str, history: list[RoundResult], money: int.
+    Potem tworzysz poker, przypisujemy do niego graczy i odtwarzamy sekwencję szykującą deck, rozdającą karty itp. Teraz player ma też przypisaną swoją hand.
+    """
