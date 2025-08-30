507f063eba278d6b0ecf88e18774ff1021027131
mkozlow6 <01187241@pw.edu.pl>
2025-08-30 13:16:11 +0200

    added game

--- lib/Player.py
+++ lib/Player.py
@@ -1,4 +1,6 @@
-enum Command:
+from enum import Enum
+
+class Command(Enum):
     FOLD = "fold"
     CALL = "call"
     RAISE = "raise"
@@ -9,12 +11,12 @@ class Action:
         self.command_type = command_type
         self.raise_amount = raise_amount
 
-enum PlayerType:
+class PlayerType(Enum):
     SMALL_BLIND = "small_blind"
     BIG_BLIND = "big_blind"
     REGULAR = "regular"
 
-enum CardRank:
+class CardRank(Enum):
     TWO = "2"
     THREE = "3"
     FOUR = "4"
@@ -28,8 +30,18 @@ enum CardRank:
     QUEEN = "Q"
     KING = "K"
     ACE = "A"
+    
+    @property
+    def value(self):
+        rank_values = {
+            CardRank.TWO: 2, CardRank.THREE: 3, CardRank.FOUR: 4, CardRank.FIVE: 5,
+            CardRank.SIX: 6, CardRank.SEVEN: 7, CardRank.EIGHT: 8, CardRank.NINE: 9,
+            CardRank.TEN: 10, CardRank.JACK: 11, CardRank.QUEEN: 12, CardRank.KING: 13,
+            CardRank.ACE: 14
+        }
+        return rank_values[self]
 
-enum CardSuit:
+class CardSuit(Enum):
     HEARTS = "hearts"
     DIAMONDS = "diamonds"
     CLUBS = "clubs"
@@ -39,6 +51,46 @@ class Card:
     def __init__(self, rank: CardRank, suit: CardSuit):
         self.rank = rank
         self.suit = suit
+    
+    def __str__(self):
+        return f"{self.rank.value}{self.suit.value}"
+    
+    def __repr__(self):
+        return self.__str__()
+
+class HandRank(Enum):
+    HIGH_CARD = 1
+    PAIR = 2
+    TWO_PAIR = 3
+    THREE_OF_A_KIND = 4
+    STRAIGHT = 5
+    FLUSH = 6
+    FULL_HOUSE = 7
+    FOUR_OF_A_KIND = 8
+    STRAIGHT_FLUSH = 9
+    ROYAL_FLUSH = 10
+
+class HandEvaluation:
+    def __init__(self, hand_rank: HandRank, value_cards: list, kicker_cards: list = None):
+        self.hand_rank = hand_rank
+        self.value_cards = value_cards  # Cards that form the hand (e.g., the pair, three of a kind)
+        self.kicker_cards = kicker_cards or []  # Remaining cards for tiebreaking
+    
+    def __lt__(self, other):
+        if self.hand_rank.value != other.hand_rank.value:
+            return self.hand_rank.value < other.hand_rank.value
+        
+        # Compare value cards first
+        for my_card, other_card in zip(self.value_cards, other.value_cards):
+            if my_card != other_card:
+                return my_card < other_card
+        
+        # Compare kickers
+        for my_kicker, other_kicker in zip(self.kicker_cards, other.kicker_cards):
+            if my_kicker != other_kicker:
+                return my_kicker < other_kicker
+        
+        return False  # Hands are equal
 
 class Perspective:
     def __init__(self, stack: list[Card], stakes: list[int], command_log: list[Command]):
@@ -51,25 +103,25 @@ class Hand:
         self.cards = cards
 
 class RoundResult:
-    def __init__(self, winner: string, pot: int, hands: list[Hand], isCardReset: bool = False):
+    def __init__(self, winner: str, pot: int, hands: list[Hand], isCardReset: bool = False):
         self.winner = winner
         self.pot = pot
         self.hands = hands
         self.isCardReset = isCardReset
 
 class Player:
-    def __init__(self, name: string, history: list[RoundResult], money: int, hand: Hand, perspective: Perspective):
+    def __init__(self, name: str, history: list[RoundResult], money: int, hand: Hand, perspective: Perspective):
         self.name = name
         self.history = history
         self.money = money
         self.hand = hand
-        self.perspective_log = list()
+        self.perspective_log = []
         self.perspective_log.append(perspective)
-
+    
     def accept(self, perspective: Perspective) -> Action:
         self.perspective_log.append(perspective)
         return self.play()
-
+    
     def play(self) -> Action:
         # Implement the logic for the player to make a move
         return Action(Command.CALL)
